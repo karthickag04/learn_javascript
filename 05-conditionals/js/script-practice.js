8645982f3b920ed10b9e2de17966173a1a430367
karthickag04 <karthickag04@gmail.com>
2025-12-19 11:30:48 +0530

    udpated with practice file

---
+++ 05-conditionals/js/script-practice.js
@@ -0,0 +1,117 @@
+//  nested while loops practice
+// var i = 1;
+// while (i <= 5) {
+//     console.log("Outer loop iteration: " + i);
+//     var j = 1;
+//     while (j <= 3) {
+//         console.log("  Inner loop iteration: " + j);
+//         j++;
+//     }
+//     i++;
+// }
+
+// //  nested for loops practice
+// for (var i = 1; i <= 5; i++) {
+//     console.log("Outer loop iteration: " + i)
+//     for (var j = 1; j <= 3; j++) {
+//         console.log("  Inner loop iteration: " + j)
+//     }
+// }
+
+
+
+
+// // while loop practice
+// var count = 1
+// while (count <= 10) {
+//     console.log("Iteration number: " + count)
+//     count++
+// }
+
+
+
+
+// // for loop practice
+// for (var i = 1; i <= 10; i++) {
+
+//     if (i % 2 === 0) {
+//         // console.log(i + " is even")
+//         // console.log("found an even number, breaking the loop")
+//         continue;
+//     }
+//     console.log("Iteration number: " + i)
+//     // if (i % 2 === 0) {
+//     //     console.log(i + " is even")
+//     //     console.log("found an even number, breaking the loop")
+//     //     break;
+//     // }
+//     // console.log("Iteration number: " + i)
+
+// }
+
+
+
+
+// var age = 25
+// var f_name = "John"
+// x = age >= 18 && f_name === "John" ? "Access granted" : "Access denied"
+// console.log(x)
+
+
+// function choiceFunction(choice) {
+
+// switch (choice) {
+//     case "a":
+//         console.log("You chose a")
+//         break;
+//     case "b":
+//         console.log("You chose b")
+//         break;
+//     case "add":
+//         console.log("You chose b")
+//         break;
+//     case 1:
+//         console.log("You chose c")
+//         break;
+//     default:
+//         console.log("Invalid choice")
+//         break;
+// }
+
+// }
+
+
+// choiceFunction("a")
+// choiceFunction("add")
+// choiceFunction("not")
+// choiceFunction(1)
+// choiceFunction("d")
+
+
+
+
+
+
+// var x = 20
+// var y = 30
+
+// if (x > y) {
+//     console.log("x is greater than y")
+// }
+// else{
+//     console.log("x is not greater than y")
+// }
+
+
+// if (x < y) {
+//     console.log("x is greater than y")
+// }
+// else if (x == y){
+//     console.log("x is equal to y")
+// }
+// else if (x == 20){
+//     console.log("x is equal to y")
+// }
+// else{
+//     console.log("x is not greater than y")
+// }

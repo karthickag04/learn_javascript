65a8d20ee42bed920cacb1a054d5a2bb5421c0b7
karthickag04 <karthickag04@gmail.com>
2026-02-04 18:56:26 +0530

    updated files

---
+++ 06-functions/js/script01.js
@@ -0,0 +1,25 @@
+// function additon(){
+//     console.log("This is addition function");
+
+// }
+// additon();
+
+// function additon(x ,y=9){
+//     console.log("This is addition function");
+//     console.log(x + y);
+//     // console.table({"name01": "John", 'name': "Doe"});
+
+// }
+// additon(5, 10);
+// additon(5, 10);
+// additon(5);
+
+
+let addition =(x,y)=> x + y;
+console.log(addition(5,10));
+
+function addition01(x,y){
+    return x + y;
+}
+
+console.log(addition01(5,10));
